7fc9c17cf217a9dd5809b28d1e77b34fa5e63ada
Ankit Kumar <149391333+Ankii04@users.noreply.github.com>
2026-01-28 19:59:43 +0530

    Time: 23 ms (83.32%), Space: 18.2 MB (40.5%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
+    bool check(int i,int j,int r,int c)
+    {
+        return i>-1 && i<r && j>-1 && j<c;
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
         int count=0;
-        vector<vector<int>>dist(r,vector<int>(c,0));
+        // vector<vector<int>>dist(r,vector<int>(c,0));
         queue<pair<int,int>>q;
         
-            for(int i=0;i<r;i++)
+            for(int l=0;l<n;l++)
             {
-                for(int j=0;j<c;j++)
+                for(int g=0;g<m;g++)
                 {
-                    if(grid[i][j]=='1' && !dist[i][j])
+                    if(grid[l][g]=='1')
                     {   count++;
-                        q.push({i,j});
-                        dist[i][j]=1;
+                        q.push({l,g});
+                        // dist[i][j]=1;
+                        grid[l][g]='0';
                
-                        int ab[8] = {-1,0,1,0};
-                        int ac[8] = {0,1,0,-1};
+                        int r[8] = {-1,0,0,1};
+                        int c[8] = {0,-1,1,0};
                         
                         while(!q.empty())
                         {
-                            int rc = q.front().first;
-                            int rd = q.front().second;
+                            int i = q.front().first;
+                            int j = q.front().second;
                             
                             
                             q.pop();
                             for(int k =0;k<8;k++)
                             {
-                            int nr = rc + ab[k];
-                            int nc = rd + ac[k];
+                            // int nr = rc + ab[k];
+                            // int nc = rd + ac[k];
                             
-                            if(nr>=0 && nr<r && nc>=0 && nc<c && grid[nr][nc]=='1' && !dist[nr][nc])
+                           if(check(i+r[k],j+c[k],n,m) && grid[i+r[k]][j+c[k]]=='1')
                             {
-                                dist[nr][nc]=1;
-                                q.push({nr,nc});
+                                grid[i+r[k]][j+c[k]]='0';
+                                q.push({i+r[k],j+c[k]});
                             }
                         }
                     }

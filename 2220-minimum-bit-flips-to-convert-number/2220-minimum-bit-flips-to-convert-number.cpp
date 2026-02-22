22d6277af561d619a33b94d2b345f669109db982
Ankit Kumar <149391333+Ankii04@users.noreply.github.com>
2026-02-22 18:06:55 +0530

    Time: 0 ms (100%), Space: 7.9 MB (91.92%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         int ans = start^goal;
-        int cnt = 0;
-        for(int i=0;i<32;i++)
-        {
-            if(ans & (1<<i))
-            {
-                cnt+=1;
-            }
-        }
+        // int cnt = 0;
+        // for(int i=0;i<32;i++)
+        // {
+        //     if(ans & (1<<i))
+        //     {
+        //         cnt+=1;
+        //     }
+        // }
+        // return cnt;
+        int cnt = __builtin_popcount(ans);
         return cnt;
     }
 };

28ae5b368abea5f16a10e1343939b77a7cc5da9e
Ankit Kumar <149391333+Ankii04@users.noreply.github.com>
2026-07-16 18:52:21 +0530

    Time: 34 ms (17.44%), Space: 24.2 MB (20.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,54 +1,50 @@
 class Solution {
 public:
-    bool check(int i,int j,int r,int c)
-    {
-        return i>-1 && i<r && j>-1 && j<c;
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int count=0;
-        // vector<vector<int>>dist(r,vector<int>(c,0));
-        queue<pair<int,int>>q;
-        
-            for(int l=0;l<n;l++)
-            {
-                for(int g=0;g<m;g++)
-                {
-                    if(grid[l][g]=='1')
-                    {   count++;
-                        q.push({l,g});
-                        // dist[i][j]=1;
-                        grid[l][g]='0';
-               
-                        int r[8] = {-1,0,0,1};
-                        int c[8] = {0,-1,1,0};
-                        
-                        while(!q.empty())
-                        {
-                            int i = q.front().first;
-                            int j = q.front().second;
-                            
-                            
-                            q.pop();
-                            for(int k =0;k<8;k++)
-                            {
-                            // int nr = rc + ab[k];
-                            // int nc = rd + ac[k];
-                            
-                           if(check(i+r[k],j+c[k],n,m) && grid[i+r[k]][j+c[k]]=='1')
-                            {
-                                grid[i+r[k]][j+c[k]]='0';
-                                q.push({i+r[k],j+c[k]});
-                            }
-                        }
-                    }
+    void bfs(vector<vector<char>>& grid, int row, int col, vector<vector<int>>& vis) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, 1, 0, -1};
+
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        vis[row][col] = 1;
+
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + delrow[i];
+                int nc = c + delcol[i];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
+                    grid[nr][nc] == '1' && !vis[nr][nc]) {
+                    vis[nr][nc] = 1;
+                    q.push({nr, nc});
                 }
+            }
         }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        int cnt = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    cnt++;
+                    bfs(grid, i, j, vis);
+                }
+            }
         }
-        
-        
-        return count;
-        
+
+        return cnt;
     }
 };

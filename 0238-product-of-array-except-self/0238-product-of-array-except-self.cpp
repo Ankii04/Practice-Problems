e2addbeb499eaf1279d3634575efd1bae56e4955
Ankit Kumar <149391333+Ankii04@users.noreply.github.com>
2026-07-20 14:21:31 +0530

    Time: 4 ms (21.09%), Space: 40 MB (96.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,52 +2,19 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        int count = 0;
-        int without_zero = 1;
-        for(int i=0;i<nums.size();i++)
+        vector<int>ans(n);
+        ans[0]=1;
+        for(int i=1;i<n;i++)
         {
-            if(nums[i]==0)
-            {
-                count++;
-            }
-            else
-            {
-                without_zero*=nums[i];
-            }
+            ans[i] = ans[i-1] * nums[i-1];
         }
-        vector<int>result(n);
-        for(int i=0;i<n;i++)
+
+        int suffix = 1;
+        for(int i=n-1;i>=0;i--)
         {
-            if(nums[i]!=0)
-            {
-                if(count>0)
-                {
-                    result[i] = 0;
-                }
-                else
-                {
-                    result[i]=without_zero/nums[i];
-                }
-            }
-            else if(count == 1) 
-            { 
-                if(nums[i] == 0) 
-                    result[i] = without_zero; 
-                else 
-                    result[i] = 0; 
-            } 
-            else
-            {
-                if(count>0)
-                {
-                    result[i] = 0;
-                }
-                else
-                {
-                    result[i]=without_zero/nums[i];
-                }
-            }
+            ans[i]*=suffix;
+            suffix*=nums[i];
         }
-        return result;
+        return ans;
     }
 };
